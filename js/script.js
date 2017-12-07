6d45a072cf86681b4dc9e1ac33996cac467b0219
AlanLOLs96 <alanlal249@gmail.com>
2017-12-08 01:12:23 +0530

    Initial Commit - Header Design

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(window).scroll(function(){
+    var wScroll = $(this).scrollTop()
+
+    $('header').css({
+        'background-position' : `center ${wScroll*.5}px`
+     })
+    console.log(wScroll)
+});
