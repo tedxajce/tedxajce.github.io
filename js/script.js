7750e737513f4f82b4affd817308bb5601b219a0
AlanLOLs96 <alanlal249@gmail.com>
2017-12-16 17:52:12 +0530

    mobile friendly

--- js/script.js
+++ js/script.js
@@ -4,5 +4,20 @@ $(window).scroll(function(){
     $('header').css({
         'background-position' : `center ${wScroll*.45}px`
      })
-    // console.log(wScroll)
+    console.log(wScroll)
+
+    if(wScroll >= 590 ){
+        $('nav.navbar.navbar-expand-lg').css({
+            'position':'fixed',
+            'top':`0`,
+            'width': `100%`,
+            'background': `rgba(0, 0, 0, 0.9)`
+        })
+    }else{
+        $('nav.navbar.navbar-expand-lg').css({
+            'position':'relative',
+            'width': `100%`,
+            'background': `rgba(0, 0, 0, 0.6)`
+        })
+    }
 });
