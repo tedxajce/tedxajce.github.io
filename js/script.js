bd21c349288948d9721bcbd34b46d320c0e6095e
alanlal <alanlal249@gmail.com>
2017-12-24 17:22:07 +0530

    menubar corrected for mobile and desktop

--- js/script.js
+++ js/script.js
@@ -1,23 +1,36 @@
-$(window).scroll(function(){
-    var wScroll = $(this).scrollTop()
+$(window).scroll(function() {
+  var wScroll = $(this).scrollTop();
 
-    $('header').css({
-        'background-position' : `center ${wScroll*.45}px`
-     })
-    console.log(wScroll)
+  $("header").css({
+    "background-position": `center ${wScroll * 0.45}px`
+  });
 
-    if(wScroll >= 590 ){
-        $('nav.navbar.navbar-expand-lg').css({
-            'position':'fixed',
-            'top':`0`,
-            'width': `100%`,
-            'background': `rgba(0, 0, 0, 0.7)`
-        })
-    }else{
-        $('nav.navbar.navbar-expand-lg').css({
-            'position':'relative',
-            'width': `100%`,
-            'background': `rgba(0, 0, 0, 0.7)`
-        })
+  console.log(wScroll);
+
+  // if(wScroll >= 590 ){
+  //     $('nav.navbar.navbar-expand-lg').css({
+  //         'position':'fixed',
+  //         'top':`0`,
+  //         'width': `100%`,
+  //         // 'background': `rgba(0, 0, 0, 0.7)`
+  //     })
+  // }else{
+  //     $('nav.navbar.navbar-expand-lg').css({
+  //         'position':'relative',
+  //         'width': `100%`,
+  //         // 'background': `rgba(0, 0, 0, 0.7)`
+  //     })
+  // }
+});
+
+var btnclicked = 0;
+$("button.navbar-toggler").click(function() {
+    if (btnclicked == 0) {
+        $("nav").css({"background": `rgba(0,0,0,.8)`});
+        btnclicked = 1;
+    } else {
+        $("nav").css({"background": `none`});
+        btnclicked = 0;
     }
+    
 });
