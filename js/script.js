83f27758f755ce27c0244db27ba34d8836bebd35
alanlal <alanlal249@gmail.com>
2017-12-29 10:13:52 +0530

    Card Layout without background

--- js/script.js
+++ js/script.js
@@ -1,36 +1,74 @@
-$(window).scroll(function() {
-  var wScroll = $(this).scrollTop();
+$(document).ready(function() {
+  $(window).scroll(function() {
+    var wScroll = $(this).scrollTop();
 
-  $("header").css({
-    "background-position": `center ${wScroll * 0.45}px`
+    $("header").css({
+      "background-position": `center ${wScroll * 0.45}px`
+    });
   });
 
-  console.log(wScroll);
-
-  // if(wScroll >= 590 ){
-  //     $('nav.navbar.navbar-expand-lg').css({
-  //         'position':'fixed',
-  //         'top':`0`,
-  //         'width': `100%`,
-  //         // 'background': `rgba(0, 0, 0, 0.7)`
-  //     })
-  // }else{
-  //     $('nav.navbar.navbar-expand-lg').css({
-  //         'position':'relative',
-  //         'width': `100%`,
-  //         // 'background': `rgba(0, 0, 0, 0.7)`
-  //     })
-  // }
-});
-
-var btnclicked = 0;
-$("button.navbar-toggler").click(function() {
+  var btnclicked = 0;
+  $("button.navbar-toggler").click(function() {
     if (btnclicked == 0) {
-        $("nav").css({"background": `rgba(0,0,0,.8)`});
-        btnclicked = 1;
+      $("nav").css({ background: `rgba(0,0,0,.8)` });
+      btnclicked = 1;
     } else {
-        $("nav").css({"background": `none`});
-        btnclicked = 0;
+      $("nav").css({ background: `none` });
+      btnclicked = 0;
     }
-    
+  });
+
+  var tedlogo = $("#about-logo-ted");
+  var tedxlogo = $("#about-logo-tedx");
+  var tedxajcelogo = $("#about-logo-tedxajce");
+
+  var ted = $("#content-ted");
+  var tedx = $("#content-tedx");
+  var tedxajce = $("#content-tedxajce");
+
+  tedx.hide();
+  tedxajce.hide();
+
+  tedlogo.click(function() {
+    tedlogo.css({
+      filter: `grayscale(0) opacity(1)`
+    });
+    tedxlogo.css({
+      filter: `grayscale(1) opacity(0.7)`
+    });
+    tedxajcelogo.css({
+      filter: `grayscale(1) opacity(0.7)`
+    });
+    ted.show();
+    tedx.hide();
+    tedxajce.hide();
+  });
+  tedxlogo.click(function() {
+    tedlogo.css({
+      filter: `grayscale(1) opacity(0.7)`
+    });
+    tedxlogo.css({
+      filter: `grayscale(0) opacity(1)`
+    });
+    tedxajcelogo.css({
+      filter: `grayscale(1) opacity(0.7)`
+    });
+    ted.hide();
+    tedx.show();
+    tedxajce.hide();
+  });
+  tedxajcelogo.click(function() {
+    tedlogo.css({
+      filter: `grayscale(1) opacity(.7)`
+    });
+    tedxlogo.css({
+      filter: `grayscale(1) opacity(.7)`
+    });
+    tedxajcelogo.css({
+      filter: `grayscale(0) opacity(1)`
+    });
+    ted.hide();
+    tedx.hide();
+    tedxajce.show();
+  });
 });
